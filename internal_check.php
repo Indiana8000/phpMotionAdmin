2903ffc101eacae5b0bc94b7ab6bbd5e9feaa2df
Andreas Kreisl <andy@Andreas-MBP-3.fritz.box>
2017-02-16 14:14:15 +0100

    Moved: ActivityLock out of loop

--- internal_check.php
+++ internal_check.php
@@ -6,11 +6,11 @@ checkMotionStatus();
 if(isset($argv) && $GLOBALS['CONFIG']['CheckActive'] == "FALSE") {
 	$stmt = $GLOBALS['DB']->prepare("SELECT eCam, eNum FROM events WHERE eType = 'EVENT START' AND eData = 'remove'");
 	if($stmt->execute()) {
+		if($GLOBALS['CONFIG']['CheckActive'] == "FALSE") {
+			$GLOBALS['DB']->query("REPLACE INTO config (cName,cValue) VALUES ('CheckActive', 'TRUE')");
+			$GLOBALS['CONFIG']['CheckActive'] = "TRUE";
+		}
 		while($row = $stmt->fetch()) {
-			if($GLOBALS['CONFIG']['CheckActive'] == "FALSE") {
-				$GLOBALS['DB']->query("REPLACE INTO config (cName,cValue) VALUES ('CheckActive', 'TRUE')");
-				$GLOBALS['CONFIG']['CheckActive'] = "TRUE";
-			}
 			$stmt2 = $GLOBALS['DB']->prepare("SELECT eFile FROM events WHERE eCam = :cid AND eNUm = :eid AND (eType = 'PICTURE' OR eType = 'MOVIE END')");
 			$stmt2->bindValue(':cid', $row['eCam'], PDO::PARAM_INT);
 			$stmt2->bindValue(':eid', $row['eNum'], PDO::PARAM_INT);
